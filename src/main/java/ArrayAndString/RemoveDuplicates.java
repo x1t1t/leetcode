c554c567c7b9945df54ac79781e865f4ca46b37c
xiangyt <xiangyt@mail.ustc.edu.cn>
2019-01-20 19:30:49 +0800

    add pom

--- src/main/java/ArrayAndString/RemoveDuplicates.java
+++ src/main/java/ArrayAndString/RemoveDuplicates.java
@@ -2,17 +2,23 @@ package ArrayAndString;
 
 import java.util.Arrays;
 
+/**
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ */
+
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] a = {0,0,1,1,1,2,2,3,3,4};
+        int[] a = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         int c = removeDuplicates(a);
         System.out.println(c);
         System.out.println(Arrays.toString(a));
     }
+
     public static int removeDuplicates(int[] nums) {
         int j = 1;
-        for (int i =1; i<nums.length; i++){
-            if (nums[i] != nums[i-1]) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
                 nums[j] = nums[i];
                 j++;
             }
