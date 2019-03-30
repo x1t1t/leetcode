62d63d88479d780e226ec9b16da4728275314b87
onet <xiangyt@mail.ustc.edu.cn>
2019-03-30 21:27:15 +0800

    20190330

---
+++ src/main/java/sortandsearch/MediumFindPeakElement.java
@@ -0,0 +1,30 @@
+package sortandsearch;
+
+/**
+ * Find Peak Element
+ * <p>
+ * 峰值元素是指其值大于左右相邻值的元素。
+ * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+ * 数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+ * 你可以假设 nums[-1] = nums[n] = -∞。
+ */
+public class MediumFindPeakElement {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(nums));
+    }
+
+    public static int findPeakElement(int[] nums) {
+        if (nums.length < 1) return -1;
+        int s = 0, e = nums.length - 1;
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                e = mid;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return s;
+    }
+}
