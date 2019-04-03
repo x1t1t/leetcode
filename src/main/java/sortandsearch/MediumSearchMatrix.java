8ea57e6fe76273989dc66394a3911d491846b4dc
onet <xiangyt@mail.ustc.edu.cn>
2019-04-03 23:48:50 +0800

    20190403

---
+++ src/main/java/sortandsearch/MediumSearchMatrix.java
@@ -0,0 +1,39 @@
+package sortandsearch;
+
+/**
+ * Search a 2D Matrix II
+ * <p>
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ */
+public class MediumSearchMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        System.out.println(searchMatrix(matrix, 5));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length;
+        if (r < 1) return false;
+        int c = matrix[0].length;
+        if (c < 1) return false;
+        int i = 0, j = c - 1;
+        while (i < r && j >= 0) {
+            if (matrix[i][j] > target) {
+                j--;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
