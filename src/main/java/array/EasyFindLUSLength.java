848f470adc1dc83df9750a8cd96a9069608def39
onet <xiangyt@mail.ustc.edu.cn>
2020-02-15 21:21:27 +0800

    20200215

---
+++ src/main/java/array/EasyFindLUSLength.java
@@ -0,0 +1,30 @@
+package array;
+
+/**
+ * Longest Uncommon Subsequence I
+ * 给定两个字符串，你需要从这两个字符串中找出最长的特殊序列。最长特殊序列定义如下：该序列为某字符串独有的最长子序列（即不能是其他字符串的子序列）。
+ * <p>
+ * 子序列可以通过删去字符串中的某些字符实现，但不能改变剩余字符的相对顺序。空序列为所有字符串的子序列，任何字符串为其自身的子序列。
+ * <p>
+ * 输入为两个字符串，输出最长特殊序列的长度。如果不存在，则返回 -1。
+ */
+public class EasyFindLUSLength {
+
+    public static void main(String[] args) {
+        System.out.print(findLUSLength("", ""));
+    }
+
+    private static int findLUSLength(String a, String b) {
+        if (a == null || a.isEmpty()) {
+            return (b == null || b.isEmpty()) ? -1 : b.length();
+        }
+        if (b == null || b.isEmpty()) {
+            return a.length();
+        }
+        if (a.length() == b.length()) {
+            return a.equals(b) ? -1 : a.length();
+        } else {
+            return Math.max(a.length(), b.length());
+        }
+    }
+}
